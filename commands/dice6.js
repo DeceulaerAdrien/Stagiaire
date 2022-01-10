928ea33b31604da75f268f6a52d017b0103ea3f4
DeceulaerAdrien <adriendeceulaer@gmail.com>
2022-01-10 18:17:19 +0100

    D6 simple ok

--- commands/dice6.js
+++ commands/dice6.js
@@ -1,17 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
-let dice = () => {
-  min = Math.ceil(1);
-  max = Math.floor(6);
-  result = Math.floor(Math.random() * (max - min + 1)) + min;
-  return result;
-};
-
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("r")
+    .setName(`r`)
     .setDescription("Replies with a number between 1 and 6"),
   async execute(interaction) {
-    await interaction.reply(dice());
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+    await interaction.reply(`${result}`);
   },
 };
