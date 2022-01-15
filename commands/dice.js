1caa1bce36813b6f52a138821df7b2a1f18dea46
DeceulaerAdrien <adriendeceulaer@gmail.com>
2022-01-15 23:26:44 +0100

    aleatoire test

--- commands/dice.js
+++ commands/dice.js
@@ -13,11 +13,8 @@ dice = (numberOfDice, maxvalue) => {
   for (let i = 0; i < resultArray.length; i++) {
     sum += resultArray[i];
   }
-  console.log(sum);
   return `${resultArray} = ${sum}`;
 };
-
-console.log(dice(100, 100));
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -27,13 +24,36 @@ module.exports = {
       option
         .setName("number_of_dice")
         .setDescription("Number of dice you want rolled")
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("face_of_dice")
+        .setDescription("face of dice you want rolled")
     ),
 
   async execute(interaction) {
     if (!interaction.isCommand()) return;
 
+    numberOfDice = interaction.options.getInteger("number_of_dice");
+    faceOfDice = interaction.options.getInteger("face_of_dice");
+
+    if (numberOfDice < 1) {
+      numberOfDice = 1;
+    }
+
+    if (faceOfDice < 6) {
+      faceOfDice = 6;
+    }
+
     if (interaction.commandName === "roll") {
-      await interaction.reply(dice(10, 6));
+      await interaction.reply(
+        `the user ${
+          interaction.user.tag
+        } roll ${numberOfDice} dice ${faceOfDice} and obtained : ${dice(
+          numberOfDice,
+          faceOfDice
+        )}`
+      );
     }
   },
 };
